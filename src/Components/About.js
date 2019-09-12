a4a4428de11d405a30d9014929a3e538d88a052b
Jason Brunelle <jason@jasonbrunelle.com>
2019-09-12 15:07:07 -0500

    About me is more responsive.

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,21 +4,25 @@ const About = () => {
   return (
     <div className='bg-image about-image'>
       <div className='container'>
-        <div className='jumbotron'>
+        <div className='jumbotron mx-auto'>
           <h1 className='display-4'>About Me</h1>
           <p className='lead'>
+            I discovered my love of computers at a young age. I've been
+            programming since the age of 11 and setting up Linux and Windows
+            machines since the age of 19. 20+ years later I have as much passion
+            as I ever had. With 15 years management experience on top of this, I
+            bring a level of maturity lacking in many recent college graduates.
+          </p>
+          <p className='show-md'>
             I recently graduated with a degree in Computer Science from the
             University of Houston Downtown at which point I relocated to
             Chicago, Illinois. Full stack web development combines my passions
             for programming, system administration, and making applications that
-            can benefit anyone on their desktop or mobile.
-          </p>
-          <hr className='my-4' />
-          <p>
-            It has been a joy to watch as JavaScript grew from a language we
-            tolerated because we didn't have anything better into a language
-            that is sought out even for back-end development. I embrace
-            functional programming concepts and higher-order functions.
+            can benefit anyone on their desktop or mobile. It has been a joy to
+            watch as JavaScript grew from a language we tolerated because we
+            didn't have anything better into a language that is sought out even
+            for back-end development. I embrace functional programming concepts
+            and higher-order functions.
           </p>
         </div>
       </div>
