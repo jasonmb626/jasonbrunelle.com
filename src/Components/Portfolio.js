b4dc70cf8a307f80fe8bf2aac0f1aeae22b5a3bf
Jason Brunelle <jason@jasonbrunelle.com>
2020-01-11 01:41:45 -0600

    Add LMS to portfolio. Add GitHub link for TexasHouseWitness

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -2,37 +2,65 @@ import React from 'react';
 
 const Portfolio = () => {
   return (
-    <div className='bg-image portfolio-image'>
-      <div className='container-fluid full-width h-100'>
-      <div className='jumbotron'>
+    <div className="bg-image portfolio-image">
+      <div className="container-fluid full-width h-100">
+        <div className="jumbotron">
           <h3>Welcome to my portfolio!</h3>{' '}
-          <h6 className='mt-3'>This portfolio is JavaScript-heavy. I also have experience with C# and Java (I practice HackerRank challenges in these languages) as well as C and C++ in school. My full projects tend towards JavaScript at this time. These projects do reinvent the wheel a bit, but have been a great chance to learn new skills.</h6>
+          <h6 className="mt-3">
+            This portfolio is JavaScript-heavy. I also have experience with C#
+            and Java (I practice HackerRank challenges in these languages) as
+            well as C and C++ in school. My full projects tend towards
+            JavaScript at this time. These projects do reinvent the wheel a bit,
+            but have been a great chance to learn new skills.
+          </h6>
         </div>
-        <div className='jumbotron'>
+        <div className="jumbotron">
           <h3>Texas House Witness</h3>{' '}
-          <h5>Search a scraped version of the affidavit records from Texas Legislature Online (TLO).</h5>
-          <h5 className='mt-3'>This project explores</h5>
+          <h5>
+            Search a scraped version of the affidavit records from Texas
+            Legislature Online (TLO).
+          </h5>
+          <h5 className="mt-3">This project explores</h5>
           <ul>
             <li>Data scraping</li>
             <li>Database Design and implementation using raw SQL</li>
             <li>Back-End API built with Node & Express</li>
             <li>Front-end UX/UI.</li>
           </ul>
-          <h6>I specifically wanted to use this project to stay as low-level as possible regarding technologies. All UI manipulation is plain JavaScript without frameworks. I didn't even use the DataTables plugin. I wanted to have a descent grasp of what frameworks really do before utilizing one.</h6>
-          <h6>Other projects utilze NoSQL with ODMs or SQL with ORMs as well as use of front-end frameworks.</h6>
+          <h6>
+            I specifically wanted to use this project to stay as low-level as
+            possible regarding technologies. All UI manipulation is plain
+            JavaScript without frameworks. I didn't even use the DataTables
+            plugin. I wanted to have a descent grasp of what frameworks really
+            do before utilizing one.
+          </h6>
+          <h6>
+            Other projects utilze NoSQL with ODMs or SQL with ORMs as well as
+            use of front-end frameworks.
+          </h6>
           <a
-            href='https://texashousewitness.info'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://texashousewitness.info"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            Visit Link
+            Visit The Site
+          </a>{' '}
+          or the{' '}
+          <a
+            href="https://github.com/jasonmb626/texashousewitness"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
           </a>
           &nbsp;to see for yourself and get more information.
         </div>
-        <div className='jumbotron'>
+        <div className="jumbotron">
           <h3>Password Keeper</h3>{' '}
-          <h5>A React Native app handling basic crud operations and encryption.</h5>
-          <h5 className='mt-3'>This project explores</h5>
+          <h5>
+            A React Native app handling basic crud operations and encryption.
+          </h5>
+          <h5 className="mt-3">This project explores</h5>
           <ul>
             <li>React Native & Expo</li>
             <li>React's Context API</li>
@@ -41,19 +69,25 @@ const Portfolio = () => {
             <li>Cryptography/Security</li>
           </ul>
           <a
-            href='https://github.com/jasonmb626/Password_Keeper_React_Native'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/jasonmb626/Password_Keeper_React_Native"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Visit the GitHub
           </a>
           &nbsp;to see for yourself and get more information.
         </div>
-        <div className='jumbotron'>
+        <div className="jumbotron">
           <h3>Job Dashboard</h3>{' '}
-          <h5>Custom full stack web app to keep track of jobs applied to, who to follow up with, etc.</h5>
-          <h6>This project gave me plenty of experience debugging and refactoring code.</h6>
-          <h5 className='mt-3'>This project explores</h5>
+          <h5>
+            Custom full stack web app to keep track of jobs applied to, who to
+            follow up with, etc.
+          </h5>
+          <h6>
+            This project gave me plenty of experience debugging and refactoring
+            code.
+          </h6>
+          <h5 className="mt-3">This project explores</h5>
           <ul>
             <li>React</li>
             <li>Redux</li>
@@ -62,18 +96,22 @@ const Portfolio = () => {
             <li>REST API with authentication</li>
           </ul>
           <a
-            href='https://github.com/jasonmb626/job_dashboard'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/jasonmb626/job_dashboard"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Visit the GitHub
           </a>
           &nbsp;to see for yourself and get more information.
         </div>
-        <div className='jumbotron'>
+        <div className="jumbotron">
           <h3>Hospital Project (Contributor)</h3>{' '}
-          <h5>Centralized website where users can get data regarding how many cancer surgeries of a given type a given hospital has performed. (In progress)</h5>
-          <h5 className='mt-3'>This project explores</h5>
+          <h5>
+            Centralized website where users can get data regarding how many
+            cancer surgeries of a given type a given hospital has performed. (In
+            progress)
+          </h5>
+          <h5 className="mt-3">This project explores</h5>
           <ul>
             <li>JQuery</li>
             <li>DataTables plugin</li>
@@ -81,35 +119,66 @@ const Portfolio = () => {
             <li>Team collaboration</li>
           </ul>
           <a
-            href='https://github.com/best-chicago-hospitals/best-chicago-hospitals-website'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/best-chicago-hospitals/best-chicago-hospitals-website"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Visit the GitHub
           </a>
           &nbsp;to see for yourself and get more information.
         </div>
-        <div className='jumbotron'>
+        <div className="jumbotron">
           <h3>Block the Blob (Collaborator)</h3>{' '}
           <h5>A game made for a school project. I was part of a team of 3.</h5>
-          <h5 className='mt-3'>This project explores</h5>
+          <h5 className="mt-3">This project explores</h5>
           <ul>
-            <li><a href="https://processing.org/" target='_blank'
-            rel='noopener noreferrer'>Processing</a> software skethbook</li>
+            <li>
+              <a
+                href="https://processing.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Processing
+              </a>{' '}
+              software skethbook
+            </li>
             <li>Team collaboration</li>
             <li>Game design</li>
           </ul>
           <a
-            href='https://github.com/jasonmb626/BlockTheBlob'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/jasonmb626/BlockTheBlob"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit the GitHub
+          </a>
+          &nbsp;to see for yourself and get more information.
+        </div>
+        <div className="jumbotron">
+          <h3>Learning Management System (Collaborator)</h3>{' '}
+          <h5>
+            A school project focused on learning to collaborate and learning
+            version control. I was part of a team of 5.
+          </h5>
+          <h5 className="mt-3">This project explores</h5>
+          <ul>
+            <li>C#</li>
+            <li>MS-SQL</li>
+            <li>Team collaboration</li>
+            <li>Testing</li>
+            <li>Software Project Management Plan Documentation</li>
+          </ul>
+          <a
+            href="https://github.com/Tavi66/3321_LMS"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Visit the GitHub
           </a>
           &nbsp;to see for yourself and get more information.
         </div>
       </div>
-    </div>    
+    </div>
   );
 };
 
