cc0bddf366ee932b79b249be7033a6b9bf2168b6
Jason Brunelle <jason@jasonbrunelle.com>
2020-02-10 14:57:08 -0600

    Fix portfolio & portfolio css. Strip out icon font.

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
 const screen_lines_element = document.getElementById('screen-lines');
+const additionalTechnologiesElement = document.getElementById('additional-technologies');
 const screen_text = screen_lines_element.dataset.html;
 const screen_text_length = screen_text.length;
 
+let technologyElements = null;
+let technologyElementIndex = 0;
+let numTechnologyElements;
+let fadeInInterval;
+
 let currentIndex = 0;
 let textOutputDone = false;
+
+const fadeInTechnologies = () => {
+    technologyElements = getTechnologyElements();
+    numTechnologyElements = technologyElements.length;
+    fadeInInterval = setInterval(() => {
+        additionalTechnologiesElement.appendChild(technologyElements[technologyElementIndex]);
+        technologyElementIndex++;
+        if (technologyElementIndex > numTechnologyElements - 1) {
+            clearInterval(fadeInInterval);
+        }
+    }, 80);
+}
+
+const getTechnologyElements = () => {
+    const favoriteTechnologies = [
+        {img: "JavaScript.png", alt: "JavaScript"},
+        {img: "Node.png", alt: "Node.JS"},
+        {img: "Express.png", alt: "Express.JS"},
+        {img: "React.png", alt: "React.JS"},
+        {img: "CSS.png", alt: "CSS3"},
+        {img: "HTML.png", alt: "HTML5"},
+        {img: "TypeScript.png", alt: "TypeScript"},
+        {img: "Git.png", alt: "Git"},
+        {img: "GitHub.png", alt: "GitHub"},
+        {img: "AWS.png", alt: "Amazon Web Services"},
+        {img: "Linux.png", alt: "Linux"},
+        {img: "Postgre.png", alt: "PostgreSQL"},
+        {img: "Mongo.png", alt: "MongoDB"},
+        {img: "VisualStudioCode.png", alt: "Visual Studio Code"}
+    ];
+    const technologyTemplate = document.getElementById('technology-example');
+    return favoriteTechnologies.map(technology => {
+        const technologyElement = technologyTemplate.cloneNode(true);
+        const img = technologyElement.content.querySelector('img');
+        img.src = `images/png/${technology.img}`;
+        img.alt = technology.alt;
+        return img;
+    });
+}
+
 
 const cursorInterval = () => {
     screen_lines_element.innerHTML = screen_text;
@@ -12,7 +58,7 @@ const cursorInterval = () => {
 }
 
 const intervalFunc = () => {
-    const rndTime = Math.floor(50 * Math.random()) + 25;
+    const rndTime = Math.floor(25 * Math.random()) + 25;
     if (screen_text[currentIndex] === '<') {
         while (screen_text[currentIndex] !== '>')
             currentIndex++;
@@ -23,7 +69,7 @@ const intervalFunc = () => {
     if (textOutputDone) {
         screen_lines_element.innerHTML = screen_text + '|';
         document.getElementById('disable-typing-directive').classList.toggle('no-show');
-        document.getElementById('additional-technologies').classList.toggle('no-show');
+        fadeInTechnologies();
         const monitor_screen = document.querySelector('.monitor-screen');
         if (monitor_screen.scrollHeight > monitor_screen.clientHeight) 
             document.getElementById('down-arrow').classList.remove('no-show');
@@ -48,8 +94,6 @@ document.querySelector('.monitor-screen').addEventListener('scroll', e => {
         document.getElementById('down-arrow').classList.add('no-show');
     }
 });
-
-console.dir(document.querySelector('.monitor-screen'))
 
 function getScrollPercent(documentElement) {
     var h = documentElement, 
