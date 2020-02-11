cbb070dc0d118cfc4d33c6de4f04eed94964f4bc
Jason Brunelle <jason@jasonbrunelle.com>
2020-02-11 10:41:44 -0600

    Add slide-in transition to portfolio

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+const main = document.getElementsByTagName('main')[0];
+const cards = document.querySelectorAll('.card');
+
+const addComeInClass = partial => {
+    const viewTop = main.scrollTop;
+    const viewBottom = main.scrollTop + window.innerHeight;
+    cards.forEach(card => {
+        const cardTop = card.offsetTop;
+        const cardBottom = card.offsetTop + card.offsetHeight;
+        compareTop    = partial === true ? cardBottom : cardTop,
+        compareBottom = partial === true ? cardTop : cardBottom;
+        if ((compareBottom <= viewBottom) && (compareTop >= viewTop )) {
+            card.classList.add('come-in');
+        }
+    });
+}
+
+addComeInClass(true);
+
+main.addEventListener('scroll', e => {
+    addComeInClass(true);
+});
